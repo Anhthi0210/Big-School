f24e474a517c5c38febd2bdbcfc800ca1b1c8284
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-03-06 15:36:50 +0700

    Add project files.

---
+++ Big School/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Big_School.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        public String Lecturerid { get; set; }
+        [Required]
+        [StringLength(255)]
+        public String Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoriID { get; set; }
+    }
+}
